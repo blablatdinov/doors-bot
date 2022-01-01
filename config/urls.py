70f7fd7a575f58aba8673879694106e7fd8802f2
blablatdinov <45946541+blablatdinov@users.noreply.github.com>
2022-01-02 00:24:52 +0300

    update user model (#7)

--- config/urls.py
+++ config/urls.py
@@ -4,19 +4,12 @@ from django.contrib import admin
 from django.urls import include, path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
-from config.swagger import urlpatterns as swagger_urlpattern
-
 urlpatterns = [
     path('admin/', admin.site.urls),
 
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
-
-if settings.DEBUG:
-    urlpatterns += [
-        *swagger_urlpattern,
-    ]
 
 if settings.TOOLBAR:
     urlpatterns += [
